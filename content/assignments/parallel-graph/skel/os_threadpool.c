fb102dcafee29bde41373076660fb6b7aeae3b8e
madalinazanficu <zanficumadalina@gmail.com>
2023-04-25 17:11:15 +0300

    Threadpool in progess

--- content/assignments/parallel-graph/skel/os_threadpool.c
+++ content/assignments/parallel-graph/skel/os_threadpool.c
@@ -2,28 +2,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
-/* === TASK === */
+typedef struct thread_arg_t thread_arg_t;
+struct thread_arg_t {
+    pthread_mutex_t *mutex;
+    os_threadpool_t *tp;
+};
+
+// Syncronization elements
+pthread_mutex_t q_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 /* Creates a task that thread must execute */
 os_task_t *task_create(void *arg, void (*f)(void *))
 {
-    // TODO
-    return NULL;
+    os_task_t *new_task = calloc(1, sizeof(os_task_t));
+    if (new_task == NULL) {
+        perror("Error creating task");
+        exit(1);
+    }
+
+    new_task->argument = arg;
+    new_task->task = f;
+
+    return new_task;
 }
 
 /* Add a new task to threadpool task queue */
 void add_task_in_queue(os_threadpool_t *tp, os_task_t *t)
 {
-    // TODO
+    // Get the head of the queue
+    os_task_queue_t *task_node = tp->tasks;
+
+    // Create a new node
+    os_task_queue_t *new_node = calloc(1, sizeof(os_task_queue_t));
+    new_node->task = t;
+    new_node->next = NULL;
+
+
+    // First task node added
+    pthread_mutex_lock(&q_mutex);
+    if (task_node == NULL) {
+
+        // Set the head of the queue
+        tp->tasks = new_node;
+        pthread_mutex_unlock(&q_mutex);
+        return;
+    }
+    pthread_mutex_unlock(&q_mutex);
+
+
+    // Find the last position where to add the task node
+    pthread_mutex_lock(&q_mutex);
+    while (task_node->next != NULL) {
+        task_node = task_node->next;
+    }
+    pthread_mutex_unlock(&q_mutex);
+
+
+    // Create the links between nodes
+    task_node->next = new_node;
 }
 
 /* Get the head of task queue from threadpool */
 os_task_t *get_task(os_threadpool_t *tp)
 {
-    // TODO
-    return NULL;
+    
+    pthread_mutex_lock(&q_mutex);
+    if (tp->tasks == NULL) {
+        return NULL;
+    }
+
+    os_task_queue_t *head = tp->tasks;
+    tp->tasks = tp->tasks->next;
+    return head;
+
+    pthread_mutex_unlock(&q_mutex);
 }
 
 /* === THREAD POOL === */
@@ -31,19 +86,69 @@ os_task_t *get_task(os_threadpool_t *tp)
 /* Initialize the new threadpool */
 os_threadpool_t *threadpool_create(unsigned int nTasks, unsigned int nThreads)
 {
-    // TODO
-    return NULL;
+    os_threadpool_t *tp = calloc(1, sizeof(os_threadpool_t));
+    if (tp == NULL) {
+        perror("Error creating threadpool");
+        exit(1);
+    }
+
+    tp->should_stop = 0;
+    tp->num_threads = nThreads;
+    tp->tasks = NULL;
+
+    // Allocate memory for threads
+    tp->threads = calloc(nThreads, sizeof(pthread_t));
+    thread_arg_t threads_args[nThreads];
+
+    // Syncronization elemnts
+    pthread_mutex_t mutex;
+    p_thread_mutex_init(&mutex, NULL);
+
+    // Create N threads
+    for (int i = 0; i < nThreads; i++) {
+        thread_arg_t arg;
+        arg.mutex = &mutex;
+        arg.tp = tp;
+        threads_args[i] = arg;
+
+        pthread_create(&tp->threads[i], NULL, thread_loop_function, &threads_args[i]);
+    }
+    
+    return tp;
 }
 
 /* Loop function for threads */
 void *thread_loop_function(void *args)
 {
-    // TODO
+    // Get the arguments of the current thread
+    thread_arg_t *arg = (thread_arg_t *) args;
+    pthread_mutex_t *mutex = arg->mutex;
+    os_threadpool_t *tp = arg->tp;
+
+    // Get a task from the queue and execute it
+    os_task_t *node_task = get_task(tp);
+    int index = node_task->argument;
+    node_task->task(index);
+
     return NULL;
 }
 
 /* Stop the thread pool once a condition is met */
 void threadpool_stop(os_threadpool_t *tp, int (*processingIsDone)(os_threadpool_t *))
 {
-    // TODO
+    // Wait for all threads to finish their tasks
+    int isDone = processingIsDone(tp);
+
+    // Stop the threadpool
+    if (isDone == 1) {
+        tp->should_stop = 1;
+
+        // Wait for all threads to finish
+        for (int i = 0; i < tp->num_threads; i++) {
+            pthread_join(tp->threads[i], NULL);
+        }
+    }
+
+    // TODO : Find a way to reacall processingIsDone() function
+    //       after a certain amount of time
 }
--- content/assignments/parallel-graph/skel/parallel.c
+++ content/assignments/parallel-graph/skel/parallel.c
@@ -8,11 +8,75 @@
 #include "os_threadpool.h"
 #include "os_list.h"
 
-#define MAX_TASK 100
+#define MAX_TASK 1000
 #define MAX_THREAD 4
 
 int sum = 0;
 os_graph_t *graph;
+os_threadpool_t *tp;
+
+
+
+// Function that is passed as an argument to task_create
+void sum_fun (void *arg) {
+
+    // Get the index of the node
+    int index = *(int*) arg;
+    os_node_t *node = graph->nodes[index];
+    sum += node->nodeInfo;
+
+    // Mark the node as visited
+    graph->visited[index] = 1;
+
+
+    // Submit tasks for the unvisited neighbours of the node
+    for (int i = 0; i < graph->nodes[index]->cNeighbours; i++) {
+        int neighbour = graph->nodes[index]->neighbours[i];
+        if (graph->visited[neighbour] == 0) {
+            graph->visited[neighbour] = 1;
+
+            void *arg = &neighbour;
+            os_task_t *task = task_create(sum_fun, arg);
+            add_task_in_queue(tp, task);
+        }
+    }
+}
+
+// Function that checks if all the nodes have been processed
+int processingIsDone(os_threadpool_t *tp) {
+    
+    for (int i = 0; i < graph->nCount; i++) {
+        if (graph->visited[i] == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+void traverse_graph() {
+
+    // Step 1: Create the threadpool
+    tp = threadpool_create(MAX_TASK, MAX_THREAD);
+
+    // Step 2: Create tasks for the root of each connected component
+    for (int i = 0; i < graph->nCount; i++) {
+        if (graph->visited[i] == 0) {
+            graph->visited[i] = 1;
+            
+            // Step 3: Create the task
+            void *arg = &i;
+            os_task_t *task = task_create(sum_fun, arg);
+
+            // Step 4: Submit the task to the threadpool
+            add_task_in_queue(tp, task);
+        }
+    }
+
+    // Step 5: Shutdown the threadpool using threadpool_stop
+    threadpool_stop(tp, processingIsDone);
+}
 
 int main(int argc, char *argv[])
 {
